c9ac1cd3e6bb151a37fbbaf8726f9deee26175e7
maercaestro <maercaestro@gmail.com>
2025-06-11 14:39:36 +0800

    fix react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Leaderboard from './components/Leaderboard'
 import AIControlDemo from './components/AIControlDemo'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/ai-demo" element={<AIControlDemo />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
